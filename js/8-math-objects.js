c5f2b9ba3c33c718bd46a7cf304b6ffb1d92509c
m-iwanczuk <michal@e-post.pl>
2019-03-02 14:52:07 +0100

    dodano 9-operators.js

---
+++ js/8-math-objects.js
@@ -0,0 +1,15 @@
+/* Właściwości obiektu Math */
+
+const pi = Math.PI;
+console.log(pi);
+
+const eulersConst = Math.E;
+console.log(eulersConst);
+
+/* Metody obiektu Math */
+
+let square = Math.pow(2, 16); // (podstawa, wykładnik)
+console.log(square);
+
+let randomNumber = Math.random();
+console.log(randomNumber);
