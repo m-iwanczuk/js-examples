4ba1a2595be27adf60fc438b75c90612c5aa147b
m-iwanczuk <michal@e-post.pl>
2019-03-02 12:49:35 +0100

    dodano 6-arrays.js i 7-objects.js

--- js/6-arrays.js
+++ js/6-arrays.js
@@ -1,2 +1,36 @@
 /* Tablice - "uporządkowany zbiór danych" */
 
+let names = ['Adam', 'Paweł', 'Anna'];
+console.log(names);
+
+console.log(names[0]);
+console.log(names[2]);
+// console.log(names[4]);  --przykład undefined - js nie uznaje tego za error!
+
+names[3] = 'Tomasz'; // nie stosujemy, zeby nie nabalaganic w tablicy
+console.log(names);
+
+/* "Metody tablicowe" */
+
+names.push('Julia'); // bezpieczniejszy sposób uzupełniania tablicy - nie musimy znać aktualnej komórki 
+console.log(names);
+
+console.log(names.length); // wyciąga długość tablicy
+
+console.log(names.join()); // zamieni całą tablicę na string oddzielony przecinkami
+console.log(names.join('+')); // zamieni całą tablicę na string oddzielony plusami
+console.log(names.join('')); // zamieni całą tablicę na string jako ciąg znaków
+
+console.log(names.reverse());
+
+console.log(names.sort()); // sortuje tablice wg wartosci ASCII (nie do końca alfabetycznie)
+
+
+/* Typy danych w tablicach */
+
+let newArray = [12, 'Cześć', true, null, ['Adam', 'Ola'], {name:'Łukasz'}];
+console.log(newArray);
+
+console.log(newArray[4][1]); //jak wyciągnąć wartość z tablicy w tablicy
+
+
---
+++ js/7-objects.js
@@ -0,0 +1,15 @@
+/* Obiekty */
+
+let blogPost = {
+    title: 'Andrzej Gołota na prezydenta',
+    content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga labore, enim fugiat aperiam quas deleniti, accusamus ut nemo molestiae rerum in, veritatis perferendis consectetur fugit error est atque provident impedit sed voluptates accusantium quae! Assumenda placeat iusto adipisci iste rem. Porro neque assumenda sed sunt possimus, earum recusandae atque vero!',
+    author: { 
+        name: 'Jan Kowalski', age: 51 
+    },
+    tags: ['polityka', 'dobra zmiana', 'bohater narodowy']
+}
+
+console.log(blogPost);
+console.log(blogPost.title);
+console.log(blogPost.tags[1]);
+console.log(blogPost.author.age);
